bf39a229f29925775807aef08e8f0969d9e44bc6
Dryicefox <Dryicefox@users.noreply.github.com>
2016-12-03 17:55:14 +0000

    Added main file

---
+++ pyliterm.py
@@ -0,0 +1,34 @@
+###Linux Quest
+
+###Linux Terminal Simulator for those not willing to install Linux or unable to run a Virtual Machine.
+
+###Starting Date: Saturday, December 3rd, 2016. 9:15am CST
+###Proposed Closing Date: Saturday, December 3rd, 2016. 9:00pm CST
+
+###Language: Python
+
+###Imports
+import time
+
+#Defining what is used in the Terminal
+def ssh():
+	pass
+
+def scp():
+	pass
+
+def cat():
+	pass
+
+def cd():
+	pass
+
+def cp():
+	pass
+
+def ls():
+	pass
+
+def main():
+	print("Welcome to the Linux Terminal Simulator!")
+	print("The goal of this simulator is to show you how to use basic commands in the Linux Terminal!")

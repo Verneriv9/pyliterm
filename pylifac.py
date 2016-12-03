18fb17eecd4096e76aa79163cf1e226035c5d425
jacobgus <jacobgus@users.noreply.github.com>
2016-12-03 15:35:24 -0600

    Add ssh to 10.160.1.244

--- pylifac.py
+++ pylifac.py
@@ -1,2 +1,18 @@
+do = True
 from tabulate import tabulate
-print tabulate([['Jane','Alah', ' Professor'], ['Taylor','Brown', 'TA'], ['John','Smith', 'Professor'], ['Sarah','Silver', 'Professor'], ['Seif','Tao', 'TA'], ['Lauren','Ulies', 'Professor'] , ['Martin','Wilson','TA']], headers=['First Name','Last Name', 'Position'], tablefmt='orgtbl')
+currentdir = "l"
+print("Welcome to the Texas Tech Faculty Listing")
+name = input("root@10.160.1.244: ")
+server = ['ls']
+if name in server:
+    currentdir = "server1"
+if currentdir == "server1":
+	print("Faculty.odf")
+while do == True:
+    newname = input("root@10.160.1.244: ").split()
+    if newname[0] == "less":
+        if newname[1] == "Faculty.odf":
+            print(tabulate([['Jane','Alah', ' Professor'], ['Taylor','Brown', 'TA'], ['John','Smith', 'Professor'], ['Sarah','Silver', 'Professor'], ['Seif','Tao', 'TA'], ['Lauren','Ulies', 'Professor'] , ['Martin','Wilson','TA']], headers=['First Name','Last Name', 'Position'], tablefmt='orgtbl'))
+    if newname[0] == "exit":
+        import txtechserver
+        do = False
--- txtechserver.py
+++ txtechserver.py
@@ -1,7 +1,8 @@
 do = True
 currentdir = "l"
 name = input("root@10.160.1.251: ")
-server = ['ls', '2', '3']
+server = ['ls', 'ssh root@10.160.1.244']
+
 if name in server:
 	currentdir = "server1"
 
@@ -15,11 +16,15 @@ while do == True:
 		if newname[1] == "PW.docx":
 			 print("This is File 2")
 		if newname[1] == "ClassSchedule.csv":
-			 print("This is File 3")
+			 print("Notes from IT: To connect to the faculty list, SSH into 10.160.1.244")
 	if newname[0] =="ls":
 		print("Hint.txt, PW.docx, ClassSchedule.csv")
+	if newname[0] =="ssh":
+		if newname[1] =="root@10.160.1.244":
+			import pylifac
+			do = False
 	if newname[0] =="exit":
 		##from pyliterm.py import cd():
 		do = False
-	
+
 ##import pylifac

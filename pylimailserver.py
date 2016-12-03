3711508a0d65ab933dd87fbaae95410be7c77756
jacobgus <jacobgus@users.noreply.github.com>
2016-12-03 16:02:53 -0600

    First Commit of TTU Email Service: 10.160.1.32

---
+++ pylimailserver.py
@@ -0,0 +1,18 @@
+do = True
+from tabulate import tabulate
+currentdir = "l"
+name = input("admin@10.0.1.32 ")
+server = ['ls']
+
+if name in server:
+    print("Texas Tech Mail Accounts")
+    print("------------------------")
+    print(tabulate([['Jane','Alah', ' LOCKED'], ['Taylor','Brown', 'LOCKED'], ['John','Smith', '105mb'], ['Sarah','Silver', '256mb'], ['Seif','Tao', 'LOCKED'], ['Lauren','Ulies', 'LOCKED'] , ['Martin','Wilson','LOCKED']], headers=['First Name','Last Name', 'MailBox'], tablefmt='orgtbl'))
+    print("Please Navigate to /mailbox/ to log in!")
+newname = input("root@10.160.1.244: ").split()
+if newname[0] == "cd":
+    if newname[1] =="/mailbox/":
+        print("Welcome to Texas Tech Remote Mail Server")
+        email = input("Please Enter Your Email Address: ")
+        if email == "john.smith@ttu.edu":
+            print("You're in")

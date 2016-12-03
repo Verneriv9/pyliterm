166561286350b7a0a4d749791b3001dab9b1f49f
Dryicefox <Dryicefox@users.noreply.github.com>
2016-12-03 18:24:36 +0000

    Add files via upload

--- pyliterm.py
+++ pyliterm.py
@@ -10,6 +10,9 @@
 ###Imports
 import time
 
+###The starting location
+#location = "~" ## /home/entered name
+
 #Defining what is used in the Terminal
 def ssh():
 	pass
@@ -20,8 +23,14 @@ def scp():
 def cat():
 	pass
 
-def cd():
-	pass
+def firstcd():
+	print("Time to get a moving grooving going.")
+	print("CD stands for change directory. Where would you like to go?")
+	location = input("{} ~ $".format(wholename)).split()
+	while location == "":
+		location = input("CD stands for change directory. Where would you like to go?").split()
+	nameandlocation = ("{} {} $".format(wholename, location[1]))
+	print(nameandlocation)
 
 def cp():
 	pass
@@ -29,6 +38,31 @@ def cp():
 def ls():
 	pass
 
-def main():
+def location():
+	pass
+
+def naming():
 	print("Welcome to the Linux Terminal Simulator!")
+	print("")
 	print("The goal of this simulator is to show you how to use basic commands in the Linux Terminal!")
+	print("")
+	time.sleep(.5)
+	print('''Typically in the terminal, the terminal will show you your current username and your computer's name.
+The format looks like this: username@computername "location" $''')
+	print("")
+	
+	name = input("What would you like your username for this session to be? ").split()
+	while name == "":
+		name = input("What would you like your username for this session to be? ").split()
+	print("")
+	computer = input("What would you like your computer's name for this session to be? ").split()
+	while computer == "":
+		computer = input("What would you like your computer's name for this session to be? ").split()
+	wholename = "{}@{}".format(name[0], computer[0])
+	print(wholename)
+	return wholename
+
+###Settable variables
+wholename = naming()
+location = "~"
+location = firstcd()
